066e7593055f04e840dd6b0e8376c02652e3f38c
NovaHFly <mugiviska@gmail.com>
2024-06-17 15:16:17 +0300

    feat: Plan out blog urls

--- blogicum/apps/blog/urls.py
+++ blogicum/apps/blog/urls.py
@@ -1,15 +1,72 @@
+from django.http import HttpResponse
 from django.urls import path
 
 from blog import views
 
 app_name = 'blog'
 
+profile_patterns = [
+    path(
+        'profile/<str:user_name>/',
+        lambda x, user_name: HttpResponse(f'Profile: {user_name}'),
+        name='profile',
+    ),
+    path(
+        'profile/<str:user_name>/edit/',
+        lambda x, user_name: HttpResponse(f'Edit profile: {user_name}'),
+        name='edit_profile',
+    ),
+]
+
+posts_patterns = [
+    path(
+        'posts/create/',
+        lambda x: HttpResponse('Create new post!'),
+        name='create_post',
+    ),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path(
+        'posts/<int:post_id>/edit_post/',
+        lambda x, post_id: HttpResponse(f'Edit post {post_id}'),
+        name='edit_post',
+    ),
+    path(
+        'posts/<int:post_id>/delete/',
+        lambda x, post_id: HttpResponse(f'Delete post {post_id}'),
+        name='delete_post',
+    ),
+]
+
+comment_patterns = [
+    path(
+        'posts/<int:post_id>/comment/',
+        lambda x, post_id: HttpResponse(f'Add comment to post {post_id}'),
+        name='add_comment',
+    ),
+    path(
+        'posts/<int:post_id>/edit_comment/<int:comment_id>/',
+        lambda x, post_id, comment_id: HttpResponse(
+            f'Edit comment {comment_id} at post {post_id}'
+        ),
+        name='edit_comment',
+    ),
+    path(
+        'posts/<int:post_id>/delete_comment/<int:comment_id>/',
+        lambda x, post_id, comment_id: HttpResponse(
+            f'Delete comment {comment_id} from post {post_id}'
+        ),
+        name='delete_comment',
+    ),
+]
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path(
         'category/<slug:category_slug>/',
         views.category_posts,
         name='category_posts',
     ),
+    *profile_patterns,
+    *posts_patterns,
+    *comment_patterns,
 ]
