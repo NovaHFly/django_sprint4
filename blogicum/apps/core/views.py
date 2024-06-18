3518519b6ad311e3403defc9c6e2f354ed5f5b6b
NovaHFly <mugiviska@gmail.com>
2024-06-18 10:15:16 +0300

    fix: Automatically login user after registration

--- blogicum/apps/core/views.py
+++ blogicum/apps/core/views.py
@@ -1,5 +1,22 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class Registration(CreateView):
+    template_name = 'registration/registration_form.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form: UserCreationForm) -> HttpResponse:
+        """If the form is valid, save the associated model."""
+        user = form.save()
+        login(self.request, user)
+
+        return redirect(self.success_url)
 
 
 def page_not_found(request: HttpRequest, exception: Exception) -> HttpResponse:
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import include, path, reverse_lazy
-from django.views.generic.edit import CreateView
+from django.urls import include, path
+
+from core.views import Registration
 
 handler404 = 'core.views.page_not_found'
 
@@ -12,11 +12,7 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path(
         'auth/registration/',
-        CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('blog:index'),
-        ),
+        Registration.as_view(),
         name='registration',
     ),
     path('admin/', admin.site.urls),
